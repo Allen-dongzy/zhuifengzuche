9603a83a9302efa2cb6a0c4ebdab67f39a5edac7
cbb <843569289@qq.com>
2021-06-02 16:37:59 +0800

    更改自定义头部bug

--- client/App.vue
+++ client/App.vue
@@ -1,11 +1,12 @@
 <script>
+	import Vue from 'vue'
 	export default {
 		globalData: {
 			
 		},
 		onLaunch: function() {
 			console.log('App Launch')
-			this.header();
+			this.header()
 		},
 		onShow: function() {
 			console.log('App Show')
@@ -13,32 +14,34 @@
 		onHide: function() {
 			console.log('App Hide')
 		},
-		/**
-		 * 获取顶部高度
-		 */
-		header(){
-			uni.getSystemInfo({
-				success: function(e) {
-					// #ifndef MP
-					Vue.prototype.StatusBar = e.statusBarHeight;
-					if (e.platform == 'android') {
-						Vue.prototype.CustomBar = e.statusBarHeight + 50;
-					} else {
-						Vue.prototype.CustomBar = e.statusBarHeight + 45;
-					};
-					// #endif
-					// #ifdef MP-WEIXIN
-					Vue.prototype.StatusBar = e.statusBarHeight;
-					let custom = wx.getMenuButtonBoundingClientRect();
-					Vue.prototype.Custom = custom;
-					Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
-					// #endif		
-					// #ifdef MP-ALIPAY
-					Vue.prototype.StatusBar = e.statusBarHeight;
-					Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
-					// #endif
-				}
-			})
+		methods: {
+			/**
+			 * 获取顶部高度
+			 */
+			header(){
+				uni.getSystemInfo({
+					success: function(e) {
+						// #ifndef MP
+						Vue.prototype.StatusBar = e.statusBarHeight;
+						if (e.platform == 'android') {
+							Vue.prototype.CustomBar = e.statusBarHeight + 50;
+						} else {
+							Vue.prototype.CustomBar = e.statusBarHeight + 45;
+						};
+						// #endif
+						// #ifdef MP-WEIXIN
+						Vue.prototype.StatusBar = e.statusBarHeight;
+						let custom = wx.getMenuButtonBoundingClientRect();
+						Vue.prototype.Custom = custom;
+						Vue.prototype.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+						// #endif		
+						// #ifdef MP-ALIPAY
+						Vue.prototype.StatusBar = e.statusBarHeight;
+						Vue.prototype.CustomBar = e.statusBarHeight + e.titleBarHeight;
+						// #endif
+					}
+				})
+			}
 		}
 	}
 </script>
