c7e504e0febd3d430b518717465415b7b73326f9
dzy <allen.dongzy@gmail.com>
2021-06-28 11:05:24 +0800

    fix: fix apis error

--- client/apis/rentalOrder.js
+++ client/apis/rentalOrder.js
@@ -1,7 +1,7 @@
 import request from '@/request'
 
 //订单管理分页查询
-const getOrderPageList = (data, loading = ' ') => {
+const getOrderPageList = (data, loading = '') => {
 	return request({
 		url: "rentalOrder/getOrderPageList",
 		contentType: 'application/x-www-form-urlencoded',
@@ -12,7 +12,7 @@ const getOrderPageList = (data, loading = ' ') => {
 }
 
 //获取订单管理状态列表与数量
-const getOrderStatus = (data, loading = '  ') => {
+const getOrderStatus = (data, loading = '') => {
 	return request({
 		url: "rentalOrder/getOrderStatus",
 		contentType: 'application/x-www-form-urlencoded',
