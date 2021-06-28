5152d43a911f3fb1176db55998b9447a99dcb4d5
1 <1>
2021-06-28 12:19:31 +0800

    fix: merge origin

--- client/apis/sms.js
+++ client/apis/sms.js
@@ -1,5 +1,6 @@
 import request from '@/request'
 
+
 // 发送忘记密码短信验证码
 export const sendForgotCode = (data, loading = '发送中') => {
 	return request({
@@ -15,9 +16,9 @@ export const sendForgotCode = (data, loading = '发送中') => {
 export const sendRegisterCode = (data, loading = '发送中') => {
 	return request({
 		url: "sms/sendRegisterCode",
-		contentType: 'application/x-www-form-urlencoded',
+		contentType: 'application/x-www-form-urlencoded;charset=utf-8',
 		method: 'POST', 
-		 data:data,
+		data,
 		loading
 	})
 }
