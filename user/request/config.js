31a9d60607d520bf3f37356a7b2f363149517a75
dzy <allen.dongzy@gmail.com>
2021-07-06 13:56:19 +0800

    fix: fix request apiKayword

--- user/request/config.js
+++ user/request/config.js
@@ -4,7 +4,7 @@ let config = {
 	productionHost: 'http://192.168.1.55:8201', // 正式域名
 	releaseHost: 'http://192.168.1.55:8201', // 测试域名
 	developHost: 'http://192.168.1.55:8201', // 开发域名
-	apiKeyword: 'lots-admin', // 接口关键字
+	apiKeyword: 'lots-member', // 接口关键字
 	ossUrl: 'https://zdkj-oss-bucket.oss-cn-hangzhou.aliyuncs.com/car-rental-user', // oss通用路径
 }
 
