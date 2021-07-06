e9bd5eb6a509a29adaccc3415563025af13c0e30
dzy <allen.dongzy@gmail.com>
2021-07-06 13:55:38 +0800

    feat: add login logic

--- user/pages/common/login.vue
+++ user/pages/common/login.vue
@@ -4,100 +4,132 @@
 			<image class="loginBox" :src="`${ossUrl}/common/logo.png`"></image>
 		</view>
 		<view class="topimg2">
-			<image style="height:544rpx;width: 544rpx;" :src="`${ossUrl}/common/loginbg.png`" mode=""></image>
+			<image style="height:544rpx;width: 544rpx;" :src="`${ossUrl}/common/loginbg.png`"></image>
 		</view>
 		<view class="setbox">
-			<input style="    background-color: #EFF0F3;
-			width: 86%;
-			margin: auto;
-			height: 96rpx;
-			padding-left: 20rpx;
-			border-radius: 10rpx;" type="text" value="" />
-
-
-			<view class="textTitle">{{account}}</view>
-
 			<view class="moreInpbox">
-				<view style="width: 90%;"><input :type="inpType" class="inpBox" style="width: 95%;"
-						placeholder="请填写密码" /></view>
-				<!-- <view style="width: 20%;background-color: #EFF0F3;color: #5A7EFF;font-size: 24rpx;"> -->
-				<image v-show="showpass==false" style="height: 40rpx;width: 40rpx;" :src="`${ossUrl}/common/guan.png`"
-					mode="" @click="look"></image>
-				<image v-show="showpass==true" style="height: 40rpx;width: 40rpx;" :src="`${ossUrl}/common/kai.png`"
-					mode="" @click="look"></image>
-				<!-- </view> -->
+				<view style="width: 90%;">
+					<input type="text" class="inpBox" style="width: 95%;" v-model="phone" placeholder="请填写手机号" />
+				</view>
 			</view>
-
-			<view class="textTitle">{{password}}</view>
-			<!-- <view class="textTitle" style="color: #007AFF;" @click="forget">忘记密码？</view> -->
+			<view class="line"></view>
+			<view class="moreInpbox">
+				<view style="width: 70%;">
+					<input :type="inpType" class="inpBox" style="width: 95%;" v-model="code" placeholder="请填写验证码" />
+				</view>
+				<view class="btn" @click="sendLoginCode">{{!codeKey?`${countDown}s`:'获取验证码'}}</view>
+			</view>
+			<view class="line"></view>
 			<view style="width: 86%;margin: auto;height: 96rpx;">
-				<button style="color: #007AFF;" @click="goindex()">登陆</button>
+				<button style="color: #007AFF;" @click="login">登陆</button>
 			</view>
 			<view class="selectBox">
-				<view style="width: 32rpx;height: 32rpx;" @click="selectbox">
-					<image v-show="selectType==false" style="height: 100%;width: 100%;"
+				<view style="width: 32rpx;height: 32rpx;padding-top: 6rpx;" @click="lookAgreement">
+					<image v-show="agreementType==false" style="height: 100%;width: 100%;"
 						:src="`${ossUrl}/mine/quanxian2.png`" mode=""></image>
-					<image v-show="selectType==true" style="height: 100%;width: 100%;"
+					<image v-show="agreementType==true" style="height: 100%;width: 100%;"
 						:src="`${ossUrl}/mine/quanxian1.png`" mode=""></image>
 				</view>
-				<view style="color: #262743;">已阅读并同意追风租车的<text style="color: #5A7EFF;"
+				<view style="color: #262743;margin-left:10rpx;">已阅读并同意追风租车的<text style="color: #5A7EFF;"
 						@click="$open('/pages/common/userAgreement')">《用户协议》</text> </view>
 			</view>
 		</view>
-
 	</view>
-
 </template>
 
 <script>
+	import {
+		sendLoginCode,
+		login
+	} from '@/apis/sso'
+	import {
+		throttle
+	} from '@/utils/tools'
+	import validator from 'crazy-validator'
+
 	export default {
 		data() {
 			return {
-				account: '手机号未注册！', //手机号码校验提示语句
-				password: '密码错误！', //密码校验提示语句
-				selectType: false, //协议切换
-				showpass: true, //密码眼睛切换 false 关闭  true开启
 				ossUrl: this.$ossUrl, // oss
+				agreementType: false, //协议切换
+				phone: '', // 手机号
+				code: '', // 验证码
+				codeKey: true, // 请求验证码开关
+				countDown: 60, // 倒计时
+				t: null, // 计时器占位
 			}
 		},
 		methods: {
-			selectbox() {
-				if (this.selectType) {
-					this.selectType = false
-				} else {
-					this.selectType = true
+			// 阅读协议
+			lookAgreement() {
+				this.agreementType = !this.agreementType
+			},
+			// 发送验证码
+			sendLoginCode: throttle(async function() {
+				if (!this.codeKey) return
+				const checkItem = {
+					value: this.phone,
+					rules: [{
+						type: 'required',
+						msg: '请输入手机号'
+					}, {
+						type: 'phone',
+						msg: '请输入正确的手机号'
+					}]
 				}
-			},
-			look() {
-				console.log('ppp')
-				if (this.showpass) {
-					this.showpass = false
-					this.inpType = 'password'
-				} else {
-					this.showpass = true
-					this.inpType = 'number'
+				const checkRes = validator(checkItem, this.$toast)
+				if (checkRes.status !== 1000) return
+				const params = {
+					phone: this.phone
 				}
-			},
-			forget() {
-				uni.navigateTo({
-					url: './forgetPhone',
-					animationType: 'pop-in',
-					animationDuration: 200,
-				})
-			},
-			goindex() {
-				console.log('pp')
-				uni.reLaunch({
-					url: '../home/home',
-					animationType: 'pop-in',
-					animationDuration: 200,
-				})
-			}
+				const [err, res] = await sendLoginCode(params)
+				if (err) return
+				this.codeKey = false
+				this.t = setInterval(() => {
+					this.countDown--
+					if (this.countDown <= 0) {
+						clearInterval(this.t)
+						this.codeKey = true
+						this.countDown = 60
+					}
+				}, 1000)
+			}),
+			// 登录
+			login: throttle(async function() {
+				if (!this.agreementType) {
+					this.$toast('请阅读并同意用户协议')
+					return
+				}
+				const checkItem = {
+					value: this.code,
+					rules: [{
+						type: 'required',
+						msg: '请输入验证码'
+					}, {
+						type: 'length',
+						min: 4,
+						max: 6,
+						msg: '验证码范围错误'
+					}]
+				}
+				const checkRes = validator(checkItem, this.$toast)
+				if (checkRes.status !== 1000) return
+				const params = { 
+					code: this.code,
+					username: this.phone
+				}
+				const [err, res] = await login(params)
+				console.log(res)
+				console.log(err)
+				if (err) return
+			})
 		}
 	}
 </script>
 
 <style lang="scss">
+	@import '@/static/scss/_mixin.scss';
+
 	.loginBox {
 		width: 224rpx;
 		height: 96rpx;
@@ -131,18 +163,16 @@
 
 	}
 
-	.textTitle {
+	.line {
 		color: #FE3636;
 		height: 40rpx;
 		width: 86%;
-		margin: 16rpx auto;
+		margin: 0x auto;
 	}
 
 	.selectBox {
 		display: flex;
-		align-items: center;
 		width: 86%;
-
 		margin: auto;
 		margin-top: 42rpx;
 	}
@@ -165,5 +195,12 @@
 		font-size: 24rpx;
 		color: #999999;
 		padding-left: 20rpx;
+	}
+
+	.btn {
+		width: 30%;
+		height: 96rpx;
+		@include flex-center;
+		@include font-set(24rpx, #5A7EFF, 500);
 	}
 </style>
