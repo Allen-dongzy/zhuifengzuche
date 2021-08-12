03fab31a1b59d8c60dfe4d5134192335b4726992
allen <allen.dongzy@gmail.com>
2021-08-12 14:13:53 +0800

    fix: fix iOS time error

--- user/pages/aliPage/orderDetail.vue
+++ user/pages/aliPage/orderDetail.vue
@@ -181,7 +181,8 @@
 		paymentAliPayCallback
 	} from '@/apis/aliApis'
 	import {
-		throttle
+		throttle,
+		transCommonTime
 	} from '@/utils/tools'
 	import {
 		showModal
@@ -199,8 +200,8 @@
 		computed: {
 			// 总天数
 			totalDate() {
-				const startTime = new Date(this.info.rentBeginTime).getTime()
-				const endTime = new Date(this.info.rentEndTime).getTime()
+				const startTime = new Date(transCommonTime(this.info.rentBeginTime)).getTime()
+				const endTime = new Date(transCommonTime(this.info.rentEndTime)).getTime()
 				const diff = endTime - startTime
 				return parseInt(diff / 1000 / 60 / 60 / 24)
 			},
