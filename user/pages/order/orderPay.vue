4185ce931e40f053d3d62f523f11eda0258f20aa
allen <allen.dongzy@gmail.com>
2021-08-16 11:59:31 +0800

    feat: add logic

--- user/pages/order/orderPay.vue
+++ user/pages/order/orderPay.vue
@@ -5,7 +5,12 @@
 			<view class="price">￥<text>{{price}}</text></view>
 		</view>
 		<view class="toast">*取车时支付租车押金￥{{rentalMoney || 0}}，可以取车时申请免押</view>
+		<!-- #ifdef MP-WEIXIN -->
 		<view class="btn" @click="getCodeByWxCode">支&#32;付</view>
+		<!-- #endif -->
+		<!-- #ifdef MP-ALIPAY -->
+		<view class="btn" @click="getCodeByWxCode">免押支付</view>
+		<!-- #endif -->
 	</view>
 </template>
 
@@ -16,6 +21,10 @@
 	import {
 		paymentPrecreate
 	} from '@/apis/payment'
+	import {
+		paymentAliPayFrozenMoney,
+		paymentAliPayCallback
+	} from '@/apis/aliApis'
 	import {
 		throttle
 	} from '@/utils/tools'
@@ -34,16 +43,7 @@
 			if (e && e.rentalMoney) this.rentalMoney = e.rentalMoney
 		},
 		methods: {
-			// 微信平台是否有登录能力
-			async isLoginFunc() {
-				let returnRes = ''
-				const [err, res] = await uni.getProvider({
-					service: 'oauth'
-				})
-				if (res && ~res.provider.indexOf('weixin')) returnRes = 'weixin'
-				return returnRes
-			},
-			// 授权
+			// 微信-授权
 			getCodeByWxCode: throttle(async function(index) {
 				if (!this.price) {
 					this.$toast('金额错误！')
@@ -61,7 +61,7 @@
 				if (err) return
 				this.paymentPrecreate(res.data.openid, index)
 			}),
-			// 发起支付
+			// 微信-发起支付
 			async paymentPrecreate(openId, index) {
 				const params = {
 					reflect: this.reflect,
@@ -76,12 +76,54 @@
 				if (err) return
 				this.pay(res.data.wapPayRequest)
 			},
-			// 支付
+			// 微信-支付
 			async pay(wapPayRequest) {
 				const [err, res] = await uni.requestPayment({
 					provider: 'wxpay',
 					...wapPayRequest
 				})
+				if (err) return
+				this.$toast('租车成功！')
+				uni.$emit('orderRefresh')
+				setTimeout(() => {
+					this.$open('/pages/order/order', 3)
+				}, 500)
+			},
+			// 支付宝-发起冻结
+			paymentAliPayFrozenMoney: throttle(async function() {
+				const params = {
+					orderSn: this.info.orderSn,
+					amount: this.info.orderDeposit
+				}
+				const [err, res] = await paymentAliPayFrozenMoney(params)
+				if (err || !res.data.orderStr) return
+				this.aliTradePay(res.data.orderStr)
+			}),
+			// 支付宝-冻结支付
+			aliTradePay(orderStr) {
+				my.tradePay({
+					orderStr,
+					success: (res) => {
+						this.paymentAliPayCallback(JSON.parse(res.result))
+					},
+					fail: (err) => {
+						console.log(err)
+					}
+				})
+			},
+			// 支付宝-冻结回调
+			async paymentAliPayCallback(info) {
+				const params = {
+					amount: info.alipay_fund_auth_order_app_freeze_response.amount,
+					outOrderNo: info.alipay_fund_auth_order_app_freeze_response.out_order_no,
+					authNo: info.alipay_fund_auth_order_app_freeze_response.auth_no,
+					fundAmount: info.alipay_fund_auth_order_app_freeze_response.fund_amount,
+					outRequestNo: info.alipay_fund_auth_order_app_freeze_response.out_request_no,
+					preAuthType: info.alipay_fund_auth_order_app_freeze_response.pre_auth_type,
+					creditAmount: info.alipay_fund_auth_order_app_freeze_response.credit_amount,
+					orderRecordType: 15
+				}
+				const [err, res] = await paymentAliPayCallback(params)
 				if (err) return
 				this.$toast('租车成功！')
 				uni.$emit('orderRefresh')
