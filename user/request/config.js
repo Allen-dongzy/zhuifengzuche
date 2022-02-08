fa622ba37207bb9ea74d06d7a4ad1a759b30659e
dongzhengyang <allen.dongzy@gmail.com>
2022-02-08 11:54:42 +0800

    chore: modify request url

--- user/request/config.js
+++ user/request/config.js
@@ -1,9 +1,9 @@
 let config = {
-	mode: 'production', // 项目模式 production release develop
+	mode: 'develop', // 项目模式 production release develop
 	secretKey: '', // md5通信秘钥
 	productionHost: 'https://zuche.zdkj1.cn', // 正式域名
 	releaseHost: 'https://zctest.zdkj1.cn', // 测试域名
-	developHost: 'http://192.168.1.64:8201', // 开发域名
+	developHost: 'http://192.168.2.124:8201', // 开发域名
 	apiKeyword: 'lots-member', // 接口关键字
 	ossUrl: 'http://oss.zdkj1.cn/car-rental-user', // oss通用路径
 }
